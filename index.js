7eb06a15f809beec05beb32c95f9aec8a04d0e94
namra-38 <nimra.rahim99@gmail.com>
2022-05-16 23:07:16 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+
+//  let count = 0
+//       let countEl = document.getElementById("count-el")
+//       let saveEl = document.getElementById("save-el")
+//       function increment() {
+//         count += 1
+//         countEl.textContent = count
+//       }
+
+//       function save() {
+//         let countStr = count + " - "
+//         saveEl.textContent += countStr
+//         countEl.textContent = 0
+//         count = 0
+//       }
+
+// let myAge = 24
+// let humanDogRatio = 7
+// let myDogAge = myAge * humanDogRatio
+// console.log(myDogAge)
+
+
+// let bonasPoints = 50
+// console.log(bonasPoints)
+// bonasPoints = bonasPoints + 50 
+// console.log(bonasPoints)
+// bonasPoints = bonasPoints - 75
+// console.log(bonasPoints)
+// bonasPoints = bonasPoints + 45
+// console.log(bonasPoints)
+
+
+// function countdown() {
+//   for (let i = 5; i >= 1; i--)
+//   console.log(i)
+// }
+// countdown()
+
+
+// let laps1 = 30
+// let laps2 = 35
+// let laps3 = 40
+// function sum(){
+//   let res = laps1 + laps2 + laps3
+//   console.log(res)
+// }
+// sum()
+
+
+
+// let lapscompleted = 0
+// function increment_the_lapsCompleted() {
+//   lapscompleted++
+//   console.log(lapscompleted)
+// }
+// increment_the_lapsCompleted()
+// increment_the_lapsCompleted()
+// increment_the_lapsCompleted()
